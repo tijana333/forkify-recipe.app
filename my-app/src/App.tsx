0c403c6b351966ad85ae4a58ee0db1caeaa1b795
tijana333 <150601653+tijana333@users.noreply.github.com>
2024-02-02 11:57:10 +0100

    FKY-4:Creating Theme

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,94 @@
 import React from "react";
 import "./App.css";
+import { createTheme, colors } from "@mui/material";
+import { ThemeProvider } from "@mui/system";
+import { LocalizatorProvider } from "@mui/lab";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import { spacing } from '@mui/system'
 
-function App() {
+const theme = createTheme({
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 480,
+        md: 767,
+        lg: 1200,
+        xl: 1519,
+      },
+
+      spacing: { 
+        xs: 0,
+        sm: 1.8,
+        md: 5,
+        lg: 10.25,
+        xl: 12.5,
+      
+
+  },
+},
+  
+  palette: {
+    primary: {
+      main: '#f48982',
+    },
+    secondary: {
+      main: '#fbdb89',
+    },
+
+    info:{
+      main:'#918581'
+    },
+
+    background:{
+      main:"#ffff"
+      darker:"#f9f5f3"
+    }
+
+    typography:{
+      fontFamily:['Nunito Sans, sans-serif'],
+    
+      h1:{
+      fontSize: "10px",
+      color:"#f9f5f3",
+      fontWeight:"400",
+      lineHeight: "16px"
+      letterSpacing:"1.6px"
+      }
+
+      h2: {
+        fontSize: "32.5px",
+        color:'#f48982',
+        fontWeight:"700",
+        lineHeight: "63.375px"
+        letterSpacing:"1.6px"
+      },
+      h3: {
+        fontSize: "20px",
+        color:'#f48982',
+        fontWeight:"700"
+        lineHeight:"22.4px"
+        letterSpacing:"1.6px"
+      },
+      h4: {
+        fontSize: "20px",
+        color:'#f48982',
+        fontWeight:"700"
+        lineHeight::"27.3px",
+        letterSpacing:"1.6px"
+      },
+      p: {
+        fontSize: "12px",
+        color: "#918581",
+        letterSpacing: "1.6px",
+      },
+     
+
+    }
+  },);
+
+
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
       <header className="App-header">
         <p>
@@ -13,12 +99,9 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+         </ThemeProvider>
+    />
+    >
+    >
+    >
+)
